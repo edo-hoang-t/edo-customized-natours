beaabcd9306dd4cc0ddbe58b5c7ed8d20a5aa3c2
Hoang Tran <descience.thh10@gmail.com>
2022-06-09 22:59:48 +0700

    prepare app for deployment

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,8 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      //   url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login', // we can do this since our website and API are hosted in the SAME domain
       data: {
         email,
         password
@@ -21,7 +22,7 @@ export const login = async (email, password) => {
       }, 1500);
     }
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     showAlert('error', err.response.data.message);
   }
 };
@@ -30,11 +31,13 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout'
+      url: '/api/v1/users/logout'
+      //   url: 'http://127.0.0.1:3000/api/v1/users/logout'
     });
 
     if (res.data.status === 'success') location.reload(true); // !!! Force a reload to server, not just browser cache
   } catch (err) {
+    console.log(err.response);
     showAlert('error', 'Error logging out! Try again.');
   }
 };
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -10,18 +10,16 @@ const stripe = Stripe(
 export const bookTour = async tourID => {
   try {
     // get checkout session
-    console.log('hello in book tour!');
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourID}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourID}`);
 
-    console.log(session);
+    // console.log(session);
 
     // create checkout form + charge credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
   } catch (err) {
+    console.log(err);
     showAlert('error', err);
   }
 };
